895720e23143d21424c65cbb0a016c80f6d4f1be
Thai <trinhhoangthai2005@gmail.com>
2024-07-14 15:04:35 +0700

    bai 2

--- buoi1.cpp
+++ buoi1.cpp
@@ -1,5 +1,8 @@
 #include<stdio.h>
 #include<conio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 void menu()
 {
 	printf("Bai 1");
@@ -14,6 +17,36 @@ void menu()
 	printf("\nBai 10");
 }
 
+void calculateRetirementDate(char* name, int day, int month, int year, char* gender) {
+	int current_year = 2021;
+	int current_age = current_year - year;
+	int retirement_age = 0;
+	int retirement_year;
+
+	if ((strcmp(gender, "nam") == 0 && (current_age < 18 || current_age > 62)) ||
+		(strcmp(gender, "nu") == 0 && (current_age < 18 || current_age > 60))) {
+		printf("Loi: Tuoi khong thuoc trong tuoi lao dong. Ma loi 101\n");
+		return;
+	}
+
+	if (strcmp(gender, "nam") != 0 && strcmp(gender, "nu") != 0) {
+		printf("Loi: Gioi tinh khong hop le. Ma loi errcode\n");
+		return;
+	}
+
+	if (strcmp(gender, "nam") == 0) {
+		retirement_age = 62;
+	}
+	else if (strcmp(gender, "nu") == 0) {
+		retirement_age = 60;
+	}
+
+	retirement_year = year + retirement_age;
+
+	printf("%s, gioi tinh %s, sinh ngay %02d/%02d/%04d. Hien tai (nam %d) %s da %d tuoi. Thoi gian %s duoc nghi huu la thang %02d/%04d.\n",
+		name, gender, day, month, year, current_year, name, current_age, name, month, retirement_year);
+}
+
 int main()
 {
 	int lc;
@@ -21,22 +54,41 @@ int main()
 	{
 		menu();
 		printf("\nMoi ban chon bai ");
-		scanf("%d", &lc);
+		scanf_s("%d", &lc);
 		switch (lc)
 		{
 		case 1 :
 		{
 			int a, b, c;
 			printf("Nhap vao so nguyen a : ");
-			scanf("%d", &a);
+			scanf_s("%d", &a);
 			printf("Nhap vao so nguyen b : ");
-			scanf("%d", &b);
+			scanf_s("%d", &b);
 			printf("Nhap vao so nguyen c : ");
-			scanf("%d", &c);
-			if (a - b <= 0)
+			scanf_s("%d", &c);
+			if (a - b = 0)
 				printf(" %d / (%d -%d) la khong tinh duoc ");
 			else
 				printf("%d / (%d - %d) la : ", c, a, b,c/(a-b));
+		}break;
+		case 2 :
+		{
+			char name[50];
+			int day, month, year;
+			char gender[10];
+	
+			printf("Nhap ho ten: ");
+			fgets(name, sizeof(name), stdin);
+			name[strcspn(name, "\n")] = '\0';  
+
+			printf("Nhap ngay sinh (dd mm yyyy): ");
+			scanf("%d %d %d", &day, &month, &year);
+
+			printf("Nhap gioi tinh (nam/nu): ");
+			scanf("%s", gender);
+
+			calculateRetirementDate(name, day, month, year, gender);
+
 		}break;
 
 
